44e7ad90e554ffb68ba77202f59c6a29c4aa17ad
Daniel Sanjuan <danielbacan2008@gmail.com>
2025-10-28 14:12:07 -0400

    Semi-completed

---
+++ Monster.java
@@ -0,0 +1,33 @@
+public class Monster{
+    private int arms;
+    private int buttons;
+    private int eyes;
+    private String name;
+
+    public Monster(){}
+    public Monster(int a, int b, int e, String n){}
+    public int getArms() {
+        return arms;
+    }
+    public int getButtons() {
+        return buttons;
+    }
+    public int getEyes() {
+        return eyes;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setArms(int arms) {
+        this.arms = arms;
+    }
+    public void setButtons(int buttons) {
+        this.buttons = buttons;
+    }
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
